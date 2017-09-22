19cf364abd79d82996b1f93bdb974234477d0b55
Mozy Okubo <mozy.okubo@gmail.com>
2017-09-22 13:30:28 +0900

    add sidebar

--- wp_camp/wp-content/themes/wpcamp/base.js
+++ wp_camp/wp-content/themes/wpcamp/base.js
@@ -1,11 +1,13 @@
-$(function () {
-    $("#recipe").on("click", function () {
-        $(this).next().not(":animated").slideToggle(500);
-    })
+(function ($) {
+    $(function () {
+        $("#recipe").on("click", function () {
+            $(this).next().not(":animated").slideToggle(500);
+        })
 
-    $("#page_top").on("click", function () {
-        $("html, body").animate({
-            "scrollTop": 0
-        }, 500);
+        $("#page_top").on("click", function () {
+            $("html, body").animate({
+                "scrollTop": 0
+            }, 500);
+        });
     });
-});
+}(jQuery));
--- wp_camp/wp-content/themes/wpcamp/functions.php
+++ wp_camp/wp-content/themes/wpcamp/functions.php
@@ -3,8 +3,22 @@
 function theme_scripts()
 {
 	wp_enqueue_style( 'theme-style', get_stylesheet_uri() );
-
 	wp_enqueue_script( 'theme-base-script', get_template_directory_uri() . '/base.js', array( 'jquery' ), '1.0.0', false );
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+
+function theme_widgets_init()
+{
+	register_sidebar( array(
+		'name'          => 'Sidebar',
+		'id'            => 'sidebar-1',
+		'description'   => '左のサイドバーです',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+
+add_action( 'widgets_init', 'theme_widgets_init' );
